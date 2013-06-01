523e974b010ed5c8faf3ca2a14b336cfe7c1190d
Anto Mervyn <amervyn14@gmail.com>
2013-06-01 18:23:09 +0100

    Added EF and logo

---
+++ eNovel/eNovel/Scripts/SiteMain.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $('#searchbar').change(
+            function () {
+                $(this).addClass("showprogress");
+            });
+    $('#searchbar').blur(
+            function () {
+                $(this).removeClass("showprogress");
+            });
+});
